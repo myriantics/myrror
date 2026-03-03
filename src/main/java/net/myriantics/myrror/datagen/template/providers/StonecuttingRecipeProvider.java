334641fa5836dc531590576e11a5b1b34684e2c7
myriantics <myriantics@proton.me>
2026-03-02 21:07:25 -0800

    Add StonecuttingRecipeProvider

---
+++ src/main/java/net/myriantics/myrror/datagen/template/providers/StonecuttingRecipeProvider.java
@@ -0,0 +1,23 @@
+package net.myriantics.myrror.datagen.template.providers;
+
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.StonecutterRecipe;
+import net.myriantics.myrror.datagen.template.MyrrorRecipeProvider;
+import net.myriantics.myrror.datagen.template.MyrrorRecipeSubProvider;
+import net.myriantics.myrror.util.NamedIngredient;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class StonecuttingRecipeProvider extends MyrrorRecipeSubProvider {
+    public StonecuttingRecipeProvider(MyrrorRecipeProvider provider, RecipeOutput output) {
+        super(provider, output);
+    }
+
+    public void addStonecuttingRecipe(NamedIngredient ingredient, ItemStack result, @Nullable String group) {
+        String resultName = RecipeProvider.getItemName(result.getItem());
+        ResourceLocation recipeId = this.provider.computeRecipeIdentifier("stonecutting", resultName + "_from_" + ingredient.getName());
+        this.acceptRecipe(recipeId, new StonecutterRecipe(group, ingredient.toIngredient(), result));
+    }
+}
